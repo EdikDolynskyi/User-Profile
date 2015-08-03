c20311532f599c1c2136d0b3c5a3f66a54f88f1d
Ihor Oziian <wrk.poshta@gmail.com>
2015-08-03 08:07:57 +0300

    feature-tests Create tests API

---
+++ api/controllers/TestsController.js
@@ -0,0 +1,11 @@
+/**
+ * TestsController
+ *
+ * @description :: Server-side logic for managing tests
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
+
