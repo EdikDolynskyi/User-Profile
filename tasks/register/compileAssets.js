12ac7cfb8bee091c4dbfbb9cc4ecd69d48e7615e
EdikDolynskyi <dolynskiy.eduard@gmail.com>
2015-08-04 17:01:20 +0300

    remove unneeded configs and concat libs

--- tasks/register/compileAssets.js
+++ tasks/register/compileAssets.js
@@ -1,10 +1,8 @@
 module.exports = function (grunt) {
 	grunt.registerTask('compileAssets', [
-		'clean:dev',
 		'jst:dev',
 		'less:dev',
 		'copy:dev',
-		'coffee:dev',
 		'stylus:dev',
 		'jade:dev'
 	]);
