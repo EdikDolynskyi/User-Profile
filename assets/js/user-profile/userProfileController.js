8dc38caa0c49b3b2224a982d7ca7dca970558fcc
Mykyta Semenistyi <nikeiwe@gmail.com>
2015-08-08 12:05:42 +0300

    [UP-47] user-profile: add datepicker

--- assets/js/user-profile/userProfileController.js
+++ assets/js/user-profile/userProfileController.js
@@ -3,7 +3,14 @@ var app = require('../angular-app');
 app.controller('UserProfileController', ['$scope', 'UserProfileService', userCtrl]);
 
 function userCtrl($scope, UserProfileService) {
-    resourceGetDataService.resUser().query(function( user ) {
+
+    UserProfileService.resUser().query(function( user ) {
         $scope.user = user;
     });
+
+
+	$scope.openDatepicker = function($event) {
+		$scope.opened = true;
+	};
+
 }
--- tasks/config/copy.js
+++ tasks/config/copy.js
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
 				{
 					src: ['./bower_components/bootstrap/dist/css/bootstrap.css'],
 					dest: '.tmp/public/styles/dependencies/bootstrap.css'
-				},
+				},	
 				{
 					src: ['./bower_components/angular-bootstrap/ui-bootstrap-tpls.js'],
 					dest: '.tmp/public/js/dependencies/ui-bootstrap-tpls.js'
@@ -59,8 +59,8 @@ module.exports = function(grunt) {
 				{
 					expand: true,
 					cwd: './bower_components/bootstrap/fonts',
-					src: ['**/*'],
-					dest: '.tmp/public/js/fonts'
+					src: ['**/*.*'],
+					dest: '.tmp/public/styles/fonts/'
 				}
 			]
 		},
