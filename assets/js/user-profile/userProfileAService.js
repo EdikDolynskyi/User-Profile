ef5faf84e61cf93962b9a239f294a406e7090226
EdikDolynskyi <dolynskiy.eduard@gmail.com>
2015-08-08 05:30:12 +0300

    [UP-47]profile: implement user profile page

---
+++ assets/js/user-profile/userProfileAService.js
@@ -0,0 +1,9 @@
+//angular
+//    .module('myApp', ['ngResource'])
+//    .service('resourceGetDataService', resourceGetDataService);
+//
+//function resourceGetDataService($resource) {
+//    this.resUser = function() {
+//        return $resource('http://localhost:1337/api/users/55c38b5a956240ba4c6a5f24');
+//    };
+//}
