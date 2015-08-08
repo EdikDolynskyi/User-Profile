e0902950a745784bba47a1c06c50f8e659e20f36
Mykyta Semenistyi <nikeiwe@gmail.com>
2015-08-08 11:20:12 +0300

    [UP-47] user-profile: fix browserify build

--- assets/js/user-profile/userProfileController.js
+++ assets/js/user-profile/userProfileController.js
@@ -1,9 +1,8 @@
-angular
-    .module('myApp')
-    .controller('userCtrl', ['$scope', 'resourceGetDataService', userCtrl]);
+var app = require('../angular-app');
 
+app.controller('UserProfileController', ['$scope', 'UserProfileService', userCtrl]);
 
-function userCtrl($scope, resourceGetDataService) {
+function userCtrl($scope, UserProfileService) {
     resourceGetDataService.resUser().query(function( user ) {
         $scope.user = user;
     });
