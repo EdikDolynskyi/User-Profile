097baf8d821264c33ab43bf1b8f06cc619e855cf
Ihor Oziian <wrk.poshta@gmail.com>
2015-08-11 16:06:02 +0300

    UP-48 Fixing

--- api/controllers/CvsController.js
+++ api/controllers/CvsController.js
@@ -4,16 +4,41 @@
  * @description :: Server-side logic for managing positions
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+var userPoject = [];
+
 
 module.exports = {
 
 	findOne : function(req, res){
 		servProject.getUserCV(req.param('id'), res, function(err, data){
-			if(err){
-				res.send(err);
-			}else{
-				res.send(data);
-			}
+			
+            userPoject = data;
+            
+           
+            for (var i = 0; i < userPoject.length ; i++ ){
+                servProject.getProjectTechnologies(userPoject[i], res, function(errTechnologies, dataTechnologies){
+			    console.log(dataTechnologies);
+                userPoject[i].technologiesList = dataTechnologies;
+            
+
+    //            if(err){
+    //				res.send(errTechnologies);
+    //			}else{
+    //				res.send(userPoject);
+    //			}
+              });  
+            }
+
+            console.log(userPoject);
+            
+//            if(err){
+//				res.send(err);
+//			}else{
+//				res.send(userPoject);
+//			}
 		});
+        
+      
+        
 	}
 };
--- api/services/servProject.js
+++ api/services/servProject.js
@@ -1,66 +1,61 @@
-var async=require('async');
-// var _=require('underscore');
-
+var async = require('async');
 
 module.exports = {
 
 	getUserCV:function(id, res, callback){
+        
 		Users
 			.findOne(id)
 			.populate('userCV')
 			.exec(function (err, itemCV) {
 
-				async.map(itemCV.userCV.projects,
+                async.map(itemCV.userCV.projects,
 					function (project, callback) {
 
 						Projects
-							.find({id:project})
-							.populateAll()
-							.exec(function (err, itemProj){
-
-								async.map(itemProj[0].technologies,
-									function (technology, callback) {
-
-										Technologies
-											.find({id:technology})
-											.populateAll()
-											.exec(function (errProjects, TechnItem){
-
-												//if(errTechn){return callback(errTechn)}
-												//callback(null, TechnItem);
-												console.log(TechnItem);
-											});
-
-									},
-									function (errFromIterator, results){
-										if(errFromIterator){res.serverError()}
-
-										var itemCVoJSON = itemCV.toJSON();
-
-										itemCVoJSON = results;
-										console.log(results, 'result');
-										// var clone = _.clone(itemPostToJSON);
-										// 													console.log(clone);
-										callback(err, results);
-									}
-								)
+							.findOne(project)
+							.exec(function (err, itemProject){
+                                 
+                                callback(null, itemProject);
 							});
 
 
 					},
 					function (errFromIterator, results){
-						if(errFromIterator){res.serverError()};
-
-						var itemCVoJSON = itemCV.toJSON();
-
-						itemCVoJSON = results;
-						console.log(results, 'result');
-						// var clone = _.clone(itemPostToJSON);
-						// 													console.log(clone);
+						if(errFromIterator){
+                            res.serverError();
+                        }
 						callback(err, results);
 					}
-				)
+				);
 
 			});
-	}
+	},
+    
+    getProjectTechnologies : function (projectItem, res, callback){
+        async.map(projectItem.technologies,
+            function (technology, callback) {
+
+                Technologies
+                    .findOne(technology)
+                    .exec(function (errTechn, TechnItem){
+
+                        if(errTechn){
+                            return callback(errTechn);
+                        }
+                        callback(null, TechnItem);
+                        //console.log(TechnItem);
+                    });
+
+            },
+            function (errFromIterator, results){
+                if(errFromIterator){
+                    res.serverError();
+                }
+                callback(errFromIterator, results);
+            }
+        )
+    }
+    
+    
 };
