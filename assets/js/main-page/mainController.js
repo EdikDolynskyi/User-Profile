caf5106341fe4f4df695cdd1896dc5255cbb4bc0
EdikDolynskyi <dolynskiy.eduard@gmail.com>
2015-08-27 15:51:26 +0300

    [UP-138] add user id to routes

--- assets/js/main-page/mainController.js
+++ assets/js/main-page/mainController.js
@@ -11,16 +11,12 @@ function mainCtrl($scope, service, $rootScope, $location) {
     //ownerId - id who login //userId - id of view user //adminId - id of admin
     $rootScope.ownerId = '55c38b5a956240ba4c6a5f24';
 
-    $rootScope.userId = '55c38b5a956240ba4c6a5f24';
-
-
     //set when admin login
     $rootScope.isAdmin = false;
     $rootScope.adminId = '55d6f0af768851c79c9dd781';
 
     if($rootScope.isAdmin)
         $rootScope.userId = $rootScope.adminId = '55c38b5a956240ba4c6a5f24';
-    console.log($rootScope.ownerId);
 
 
     this.search = function () {
@@ -62,16 +58,13 @@ function mainCtrl($scope, service, $rootScope, $location) {
 
 
     this.showUserPage = function (id) {
-        $rootScope.userId = id;
-        $location.path('/userdata');
+        $location.path('/userdata/' + id);
         ctrl.usersList = [];
         ctrl.searchText = '';
-        //ng-init="TabsCtrl.deactivateUserProfileTab()";
     };
 
     this.showUserPageFromFilter = function (id) {
-        $rootScope.userId = id;
-        $location.path('/userdata');
+        $location.path('/userdata/' + id);
     };
 
     this.go = function (path) {
--- assets/js/user-profile/userProfilePublicController.js
+++ assets/js/user-profile/userProfilePublicController.js
@@ -1,14 +1,16 @@
 var app = require('../angular-app');
 
-app.controller('UserProfilePublicController', ['$scope', 'UserProfileService', 'Upload', '$rootScope', userCtrl]);
+app.controller('UserProfilePublicController', ['UserProfileService', '$rootScope', '$location', userCtrl]);
 
-function userCtrl($scope, service, upload, $rootScope) {
+function userCtrl(service, $rootScope, $location) {
     var ctrl = this;
     //Init
     ctrl.today = new Date();
 
 
-    service.get($rootScope.userId, function (user) {
+    var userId = $location.path().split("/")[2]
+    console.log(userId);
+    service.get(userId, function (user) {
         ctrl.user = user;
         ctrl.userOriginal = angular.extend({}, user);
     });
