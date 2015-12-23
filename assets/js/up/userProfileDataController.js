e4ad2a0b0cca4af08f276ee31f5d6ef6be1bb5be
Edik Dolynskyi <dolynskiy.eduard@gmail.com>
2015-12-23 16:31:33 +0200

    develop: fix redirect to my profile

--- assets/js/up/userProfileDataController.js
+++ assets/js/up/userProfileDataController.js
@@ -5,9 +5,6 @@ app.controller('userProfileDataController', userProfileDataCtrl);
 function userProfileDataCtrl(prefix, $scope, $location) {
     $scope.prefix = prefix;
     var vm = this;
-    vm.goToMyProfile = function(){
-		$location.path('/profile/');
-	};
     var prefix = window.location.pathname;
     vm.tabs = [
         { title:'User Profile', content: prefix + '/js/up/up-public.html' },
